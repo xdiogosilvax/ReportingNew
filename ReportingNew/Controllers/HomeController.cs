b2df97ed1cd489417d125f5b3a37149da2df2bb8
Diogo Silva <SilvaD@quadranet.co.uk>
2021-09-22 00:26:15 +0100

    Open Report, Dropdowns populated with stored procs and Fix tree view

--- ReportingNew/Controllers/HomeController.cs
+++ ReportingNew/Controllers/HomeController.cs
@@ -7,7 +7,8 @@ using System.Data.Sql;
 using System.Data.SqlClient;
 using ReportingNew.Models;
 using System.Dynamic;
-        
+using System.Net;
+
 namespace ReportingNew.Controllers
 {
     public class HomeController : Controller
@@ -20,7 +21,6 @@ namespace ReportingNew.Controllers
 
             //return View(returnFamilies);
 
-
             var model = new SPMenuModel();
             //model.BrandsSite = context.P_Mob_Get_BrandSites_Result("john",2,1).ToList();
             //model.Brands_User = context.P_Mob_Get_BrandsForUser.ToList();
@@ -28,13 +28,43 @@ namespace ReportingNew.Controllers
             model.CategoriesReport = context.P_Mob_Get_ReportCategories("John", 1);
             model.namesReport = context.P_Mob_Get_ReportNames("John", 1,2);
 
+            model.Brands_User = context.P_Mob_Get_BrandsForUser("John");
+            model.BrandsSite = context.P_Mob_Get_BrandSites("John","The White Hart".ToString(),1);
+
+
             return View(model);
 
         }
 
 
-       
 
+        //To be changed, being used for MvP
+
+        public ActionResult urlMVP() 
+        {
+            string url =("https://testreporting.quadranet.co.uk/reports/mobilereport/YesterdaySales?rs:Embed=True&Title=False");
+
+           
+           return Redirect(url);
+        }
+
+
+
+        /*
+        public HttpWebRequest urlMVP()
+        {
+
+            HttpWebRequest request = null;
+
+            request.Method = "POST";
+            request.Credentials = new NetworkCredential("username", "password");
+
+            return redirect()request;
+
+        }
+
+
+        */
 
 
     }
