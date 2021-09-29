e47d5b76b1ec0b08d19a8eb05f9f31155f956881
Diogo Santos Silva <21814203@bucks.ac.uk>
2021-09-29 13:48:39 +0100

    URL Generator working, just user id and report id missing

--- ReportingNew/Controllers/HomeController.cs
+++ ReportingNew/Controllers/HomeController.cs
@@ -16,29 +16,62 @@ namespace ReportingNew.Controllers
     public class HomeController : Controller
     {
         WarehouseEntities context = new WarehouseEntities();
-        public ActionResult Index()
+        public ActionResult Index(Guid? userID)
         {
 
 
-            var user = Guid.Parse("01181168-6215-4050-9F46-9B1DCAA1626E");
+           // var user = userID;
             //var returnFamilies = context.P_Mob_Get_ReportFamilies("John");
             var model = new SPMenuModel();
 
 
 
+            userID = Guid.Parse("01181168-6215-4050-9F46-9B1DCAA1626E");
+            model.familiesReport = context.P_Mob_Get_ReportFamilies(userID).ToList();
+            model.CategoriesReport = context.P_Mob_Get_ReportCategories(userID, 1).ToList();
+            model.namesReport = context.P_Mob_Get_ReportNames(userID, 1, 2).ToList();
+
+
+            var family = 0;
+            var category = 0;
+
+
+            ObjectResult<P_Mob_Get_ReportFamilies_Result> families_Results = context.P_Mob_Get_ReportFamilies(userID);
+            ObjectResult<P_Mob_Get_ReportCategories_Result> categories_results = context.P_Mob_Get_ReportCategories(userID,family);
+            ObjectResult<P_Mob_Get_ReportNames_Result> report_results = context.P_Mob_Get_ReportNames(userID, family,category);
+
+            /*
+            
+            foreach(var i in families_Results.AsEnumerable())
+            {
+                i.FamilyID = family;
+
+                foreach(var c in categories_results.AsEnumerable())
+                {
+                    i.FamilyID = family;
+                    c.CategoryID = category;
+
+                    foreach(var n in report_results.AsEnumerable())
+                    {
+
+                        i.FamilyID = family;
+                        c.CategoryID = category;
+                        
+                    }
+
+
+                }
+
+                   
+            }
+
+            */
 
 
 
-            //var user = Guid.Parse("01181168-6215-4050-9F46-9B1DCAA1626E");
-            model.familiesReport = context.P_Mob_Get_ReportFamilies(user).ToList();
-            model.CategoriesReport = context.P_Mob_Get_ReportCategories(user, 1).ToList();
-            model.namesReport = context.P_Mob_Get_ReportNames(user, 1, 2).ToList();
 
 
-
-
-
-            model.sitesRep = context.P_Mob_Get_SitesForAUser(user).ToList();
+            model.sitesRep = context.P_Mob_Get_SitesForAUser(userID).ToList();
             ViewBag.Brand = model.sitesRep;
             return View(model);
 
@@ -46,33 +79,38 @@ namespace ReportingNew.Controllers
 
         }
 
-
-
-        //Test changed, being used for MvP
-
        
-        public ActionResult urlT()
+        public ActionResult urlT(Guid? user_id)
         {
-
             SPMenuModel model = new SPMenuModel();
-            var user = Guid.Parse("01181168-6215-4050-9F46-9B1DCAA1626E");
+            user_id =Guid.Parse("01181168-6215-4050-9F46-9B1DCAA1626E");
 
             var keys = Request.Form.AllKeys;
 
-            var site =Request.Form.Get(keys[0]);
-            var brand =Request.Form.Get(keys[1]);
-            var dateFrom = Request.Form.Get(keys[2]);
-            var dateTo = Request.Form.Get(keys[3]);
+            var site = Request.Form.Get(keys[0]);
+            var dateFrom = Request.Form.Get(keys[1]);
+            var dateTo = Request.Form.Get(keys[2]);
             var urlFromSP = "0";
 
+
+            var brand = 0;
+            var siteID = 0;
             P_Mob_GetReportURL_Result test = new P_Mob_GetReportURL_Result();
-            //ObjectResult<P_> objectResult_BrandID = context.P_Mob_Get_SitesForAUser(user);
+            ObjectResult<P_Mob_Get_SitesForAUser_Result> objectResult_sites = context.P_Mob_Get_SitesForAUser(user_id);
+
+            foreach(var i in objectResult_sites.AsEnumerable())
+            {
+
+                if(i.SiteName==site)
+                {
+
+                     brand = Convert.ToInt32(i.Brandid);
+                     siteID = Convert.ToInt32(i.SiteID);
+                }
+            }
 
 
-
-
-
-            ObjectResult<P_Mob_GetReportURL_Result> objectResultURL = context.P_Mob_GetReportURL(1, 1, 1, dateFrom, dateTo, user);
+            ObjectResult<P_Mob_GetReportURL_Result> objectResultURL = context.P_Mob_GetReportURL(1, brand, siteID, dateFrom, dateTo, user_id);
 
 
             foreach (var reportURL_Result in objectResultURL.AsEnumerable())
@@ -80,7 +118,6 @@ namespace ReportingNew.Controllers
                 urlFromSP = reportURL_Result.URL.ToString();
             }
 
-      
 
             var request = (HttpWebRequest)WebRequest.Create(urlFromSP);
 
@@ -92,7 +129,7 @@ namespace ReportingNew.Controllers
             var cache = new CredentialCache();
             cache.Add(new Uri(urlFromSP), "Basic", cred);
 
-            
+
             ServicePointManager.ServerCertificateValidationCallback = new
          RemoteCertificateValidationCallback
          (
